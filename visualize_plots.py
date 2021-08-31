6276767e21c61836b983bc4f4ec34b5e48bbf164
SoumyaSHU <89704220+SoumyaSHU@users.noreply.github.com>
2021-08-31 21:56:38 +0100

    Add files via upload

--- visualize_plots.py
+++ visualize_plots.py
@@ -1,8 +1,9 @@
 import seaborn as sns
+import numpy as np
 from matplotlib import pyplot, pyplot as plt
 from sklearn import metrics
-from yellowbrick.classifier.classification_report import classification_report
-from yellowbrick.classifier.rocauc import roc_auc
+from sklearn.metrics import roc_curve
+from sklearn.metrics import confusion_matrix
 
 
 def get_heatmap(df):
@@ -15,37 +16,35 @@ def get_heatmap(df):
 
 def show_sentiments(data):
     sentiment_plot = data['sentiment'].value_counts(sort=True, ascending=False).plot(kind='bar', figsize=(4, 4),
-                                                                         title='Total occurance of sentiments')
+                                                                                     title='Total occurance of sentiments')
     sentiment_plot.set_xlabel('sentiment')
     sentiment_plot.set_ylabel('frequency')
     pyplot.show()
 
 
-def roc_curves(model_name, y_test, y_pred):
-    # calculate roc curves
-    fpr, tpr, threshold = metrics.roc_curve(y_test, y_pred, pos_label=1)
+def roc_curves(y_test, lr_probs):
+    plt.plot([0, 1], [0, 1], linestyle='--', label='No Skill')
+    # calculate roc curve for model
+    fpr, tpr, _ = roc_curve(y_test, lr_probs)
+    # plot model roc curve
     roc_auc = metrics.auc(fpr, tpr)
-    print('ROC AUC=%.3f' % roc_auc)
-    plt.title('ROC Plot for Model: {}'.format(model_name))
-    plt.plot(fpr, tpr, 'b', label='AUC = %0.2f' % roc_auc)
-    plt.legend(loc='lower right')
-    plt.plot([0, 1], [0, 1], 'r--')
-    plt.xlim([0, 1])
-    plt.ylim([0, 1])
-    plt.ylabel('True Positive Rate')
+    plt.plot(fpr, tpr, marker='.', label='AUC = %0.2f' % roc_auc)
+    # axis labels
     plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    # show the legend
+    plt.legend()
+    # show the plot
     plt.show()
 
 
-def report_classification(pipeline, X_train, y_train, X_test, y_test):
-    # Plot Classification Report
-    index = ['POSITIVE', 'NEGATIVE', 'NEUTRAL']
-    report_classification = classification_report(pipeline,
-                                                  X_train, y_train,
-                                                  X_test, y_test,
-                                                  support="percent",
-                                                  cmap="Reds",
-                                                  classes=index,
-                                                  fig=pyplot.figure(figsize=(8, 6))
-                                                  )
-    return report_classification
+def cf_matrix(y_test, y_pred):
+    cf_matrix = confusion_matrix(y_test, y_pred)
+    sns.heatmap(cf_matrix / np.sum(cf_matrix), annot=True,
+                fmt='.2%', cmap='Blues')
+    pyplot.xlabel('False Positive Rate')
+    pyplot.ylabel('True Positive Rate')
+    pyplot.tight_layout()
+    return pyplot.show()
+
+
